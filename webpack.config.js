6d8169f104510068ea096d0ba0db22d0efba7e16
James Bonner <jabcmes@outlook.com>
2025-05-12 01:01:29 +1000

    setting up packages and webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/graph_pipeline.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist'),
+	},
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+			{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'
+						]
+					}
+				}
+			}
+		]
+	}
+};
